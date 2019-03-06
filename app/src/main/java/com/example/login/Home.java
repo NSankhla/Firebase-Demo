bf9998b024c0ec0af3cf201194a3cf864dc6abf5
Nirmal Sankhla <sankhla675291@gmail.com>
2019-03-06 11:33:20 +0530

    LogOut added

--- app/src/main/java/com/example/login/Home.java
+++ app/src/main/java/com/example/login/Home.java
@@ -1,13 +1,37 @@
 package com.example.login;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Home extends AppCompatActivity {
-
+Button btnLogOut;
+FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        btnLogOut=findViewById(R.id.btnLogOut);
+        auth=FirebaseAuth.getInstance();
+        btnLogOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                auth.signOut();
+                startActivity(new Intent(Home.this,Login.class));
+                finish();
+
+
+
+            }
+        });
+
+
+
     }
 }
